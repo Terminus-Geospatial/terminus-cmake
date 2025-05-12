b222b66049fb10f847cc6c8a28f0bccdd83cedbe
Marvin Smith <marvin.smith@terminus-geo.com>
2025-05-11 22:28:32 -0600

    Updating with new changlog.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 class CMakeConan(ConanFile):
 
     name = "terminus_cmake"
-    version = "1.0.5"
+    version = "1.0.6"
 
     license = "Terminus Proprietary"
     author = "Marvin Smith <marvin_smith1@me.com>"
